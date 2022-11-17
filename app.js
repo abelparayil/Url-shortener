cd8ca1b3e7f5fe35ed83cdb030059c8bcdefebc0
Abel Parayil Abraham <abelabr321@gmail.com>
2022-11-17 12:34:55 +0530

    Project complete

---
+++ app.js
@@ -0,0 +1,33 @@
+const containerPopUp = document.querySelector(".popup-container");
+const popup = document.querySelector('.popup');
+
+formUrl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = e.target.Url.value;
+    fetchUrl(value);
+});
+
+const fetchUrl = async(url) => {
+    const data = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`).then(response => response.json());
+    const ulPopup = document.createElement('ul');
+    ulPopup.setAttribute("id", "ul");
+    ulPopup.innerHTML = `
+        <li>Long URL: <a href=${data.result.original_link} target="_blank">${data.result.original_link}</a></li>
+        <li>Short URL: <a href=${data.result.full_short_link} target="_blank">${data.result.full_short_link}</a></li>
+    `
+
+    popup.append(ulPopup);
+    if (data) {
+        containerPopUp.classList.add('show');
+        url.target.value= "";
+    }
+};
+
+containerPopUp.addEventListener("click", (e) => {
+    if(e.target.classList.contains('popup-container')){
+        const ulPopup = document.getElementById('ul');
+        ulPopup.remove();
+        e.target.classList.remove("show");
+        
+    }
+})
